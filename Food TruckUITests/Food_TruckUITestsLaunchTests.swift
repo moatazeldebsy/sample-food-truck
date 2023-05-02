473e0f55268b8b61d9b2f299dc4ca06d70b60b39
Moataz Nabil <moataznabil2019@gmail.com>
2023-05-02 22:57:49 +0200

    add UI tests

---
+++ Food TruckUITests/Food_TruckUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Food_TruckUITestsLaunchTests.swift
+//  Food TruckUITests
+//
+//  Created by Moataz Nabil on 02.05.23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import XCTest
+
+final class Food_TruckUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
